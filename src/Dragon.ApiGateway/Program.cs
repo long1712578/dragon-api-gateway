5602c98005791d12b6bd30fff05d4741c5b4d17f
long1712578 <pdlong578@gmail.com>
2026-04-18 18:54:42 +0700

    fix api gateway

--- src/Dragon.ApiGateway/Program.cs
+++ src/Dragon.ApiGateway/Program.cs
@@ -5,9 +5,15 @@ using Ocelot.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration
-	.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
-	.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
-	.AddEnvironmentVariables();
+	.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+// Nếu không chạy trên Kubernetes thì mới nạp thêm đè cấu hình local
+if (Environment.GetEnvironmentVariable("KUBERNETES_SERVICE_HOST") == null)
+{
+	builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+}
+
+builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.Configure<ForwardedHeadersOptions>(options =>
 {
